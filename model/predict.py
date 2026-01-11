7954f8708b21f5a94bece93335117ba66a0528fe
mittalakshat43-lang <mittalakshat43@gmail.com>
2026-01-12 01:39:40 +0530

    Update predict.py

--- model/predict.py
+++ model/predict.py
@@ -1,1 +1,25 @@
+# model/predict.py
+
+def predict(model, X):
+    """
+    Generates BUY / NO BUY predictions.
+
+    Parameters:
+    model: trained model
+    X (pd.DataFrame): feature columns only
+
+    Returns:
+    array of predictions (0 or 1)
+    """
+    return model.predict(X)
+
+
+def predict_proba(model, X):
+    """
+    Generates BUY probability.
+
+    Returns:
+    probability of class 1 (BUY)
+    """
+    return model.predict_proba(X)[:, 1]
 
